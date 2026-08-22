78f4ab32e902cc8645a9b0dfc3112d4cf540fbe2
djkier <djjustinekier@gmail.com>
2026-08-22 12:59:36 +0800

    Initial commit

---
+++ B326/individual-fontanilla/src/com/joysistvi/stage1/day3/Activity_Fontanilla.java
@@ -0,0 +1,35 @@
+package com.joysistvi.stage1.day3;
+
+public class Activity_Fontanilla {
+    public static void main(String[] args) {
+
+        boolean isHoliday = true;
+        System.out.println("Is there a holiday tomorrow (August 21)? " + isHoliday);
+
+        byte numberOfClassmate = 24;
+        System.out.println("How many are your classmates in the training? " + numberOfClassmate);
+
+        short birthYear = 1995;
+        System.out.println("Year of Birth: " + birthYear);
+
+        int population = 250000;
+        System.out.println("Population of the city: " + population);
+
+        long worldPopulation = 8_200_000_000L;
+        System.out.println("World population: " + worldPopulation);
+
+        float eggPrice = 9.25f;
+        System.out.println("Egg price: " + eggPrice + " Pesos");
+
+        double price = 1999.99;
+        System.out.println("Price: ₱" + price);
+
+        char grade = 'A';
+        System.out.println("My grade is: " + grade);
+
+        String viand = "adobo";
+        System.out.println("Today viand is " + viand);
+
+    }
+
+}
