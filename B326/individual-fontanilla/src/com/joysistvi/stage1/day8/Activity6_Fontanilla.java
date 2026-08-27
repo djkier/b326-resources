8bd10b40ee68d728f33fa92cc8bbb5a4afb871ce
djkier <djjustinekier@gmail.com>
2026-08-27 23:23:06 +0800

    Add day 8 sample codes, Add activy 6

---
+++ B326/individual-fontanilla/src/com/joysistvi/stage1/day8/Activity6_Fontanilla.java
@@ -0,0 +1,75 @@
+package com.joysistvi.stage1.day8;
+
+import java.util.Scanner;
+
+public class Activity6_Fontanilla {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String email = "hello@gmail.com";
+        String password = "hello1234";
+
+        int maxAttempts = 3;
+        boolean isLoggedIn = false;
+
+        //for loop
+        //identify if invalid email or invalid password.
+        System.out.println("============================");
+        System.out.println("WELCOME TO JARETHS.COM");
+        System.out.println("============================");
+
+        for(int attempts = 0; attempts <= maxAttempts; attempts++) {
+            int remainingAttempts = maxAttempts - attempts;
+            if (remainingAttempts <= 0) {
+                System.out.println("Maximum attempts has been reached. Try again later.");
+                break;
+            }
+
+            //ui
+            System.out.println("Remaining Attempts: " + remainingAttempts);
+            System.out.print("Enter email: ");
+            String loginEmail = scan.nextLine();
+
+            System.out.print("Enter password: ");
+            String loginPass = scan.nextLine();
+            System.out.println("============================");
+
+            //space for new screen
+            System.out.printf("%n%n%n%n%n%n%n%n%n%n%n%n%n");
+
+            //Login conditions
+            if (!loginEmail.equals(email) && !loginPass.equals(password)) {
+                System.out.println("Wrong Email and Password");
+            } else if (!loginEmail.equals(email)) {
+                System.out.println("Wrong email. Try again.");
+            } else if (!loginPass.equals(password)) {
+                System.out.println("Wrong password. Try again.");
+            } else {
+                System.out.println("============================");
+                System.out.println("Login successfully!");
+                System.out.println("============================");
+                break;
+            }
+            System.out.println("----------------------------");
+
+
+
+        }
+
+//        while (!isLoggedIn) {
+//            System.out.print("Enter email: ");
+//            String loginEmail = scan.nextLine();
+//
+//            System.out.println("Enter password: ");
+//            String loginPass = scan.nextLine();
+//
+//            if (loginEmail.equals(email) && loginPass.equals(password)) {
+//                System.out.println("Login Successfully!");
+//
+//                isLoggedIn = true;
+//            } else {
+//                System.out.println("Invalid Username or Password!");
+//            }
+//        }
+    }
+}
---
+++ B326/individual-fontanilla/src/com/joysistvi/stage1/day8/SimpleLogin.java
@@ -0,0 +1,32 @@
+package com.joysistvi.stage1.day8;
+
+import java.util.Scanner;
+
+public class SimpleLogin {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String email = "hello@gmail.com";
+        String password = "hello1234";
+
+        boolean isLoggedIn = false;
+        while (!isLoggedIn) {
+            System.out.print("Enter email: ");
+            String loginEmail = scan.nextLine();
+
+            System.out.println("Enter password: ");
+            String loginPass = scan.nextLine();
+
+            if (loginEmail.equals(email) && loginPass.equals(password)) {
+                System.out.println("Login Successfully!");
+
+                isLoggedIn = true;
+            } else {
+                System.out.println("Invalid Username or Password!");
+            }
+        }
+
+
+    }
+}
