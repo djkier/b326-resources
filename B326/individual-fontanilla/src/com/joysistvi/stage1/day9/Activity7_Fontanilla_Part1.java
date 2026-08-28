da39bbd25d6582d180e00622d1945eebad714a16
djkier <djjustinekier@gmail.com>
2026-08-28 21:16:09 +0800

    Add activity7 part 1

---
+++ B326/individual-fontanilla/src/com/joysistvi/stage1/day9/Activity7_Fontanilla_Part1.java
@@ -0,0 +1,24 @@
+package com.joysistvi.stage1.day9;
+
+import java.util.Scanner;
+
+public class Activity7_Fontanilla_Part1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //UI
+        System.out.println("======================");
+        System.out.println("Multiplication Table");
+        System.out.println("======================");
+
+        //ask user
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        System.out.println("Multiplication Table of " + number);
+        for (int i = 1; i <= 10; i++) {
+            int product = number * i;
+            System.out.println(number + " * " + i + " = " + product);
+        }
+    }
+}
---
+++ B326/individual-fontanilla/src/com/joysistvi/stage1/day9/Activity7_Fontanilla_Part2.java
@@ -0,0 +1,4 @@
+package com.joysistvi.stage1.day9;
+
+public class Activity7_Fontanilla_Part2 {
+}
