bff503d64601763b854bafdb4b205d288bd12cae
djkier <djjustinekier@gmail.com>
2026-09-14 10:14:37 +0800

    Add Main class on activity 14

---
+++ src/com/joysistvi/stage2/day22/Activity14_Polymorphism/Main.java
@@ -0,0 +1,15 @@
+package com.joysistvi.stage2.day22.Activity14_Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = {
+                new RegularEmployee("Ana", 15000),
+                new SalesEmployee("Ben", 12000, 5000),
+                new ContractualEmployee("Cruz", 160, 75)
+        };
+
+        for (Employee e : employees) {
+            e.printPayslip();
+        }
+    }
+}
